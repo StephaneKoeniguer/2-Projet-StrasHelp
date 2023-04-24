9921beb323639e6ab5ebf97d88d9cfeece601fb0
liondor67 <fouad.tebi@gmail.com>
2023-04-24 15:54:58 +0200

    Update main.js

--- public/assets/main.js
+++ public/assets/main.js
@@ -8,17 +8,16 @@ function closeModal() {
     element.classList.remove("show-modal");
 }
 
-var menu = document.querySelector('nav ul');
-var menuBar = document.querySelector('nav .menu-icon');
-var iconMenu = document.querySelector('nav .menu-icon img');
+var menu = document.querySelector("nav ul");
+var menuBar = document.querySelector("nav .menu-icon");
+var iconMenu = document.querySelector("nav .menu-icon img");
 
-menuBar.addEventListener('click',function(){
-
-    if (iconMenu.getAttribute("src") == 'assets/menu.png') {
-        iconMenu.setAttribute("src","assets/close.png");
-    }else{
-        iconMenu.setAttribute("src","assets/menu.png");
+menuBar.addEventListener("click", function () {
+    if (iconMenu.getAttribute("src") == "assets/images/menu.png") {
+        iconMenu.setAttribute("src", "assets/images/close.png");
+    } else {
+        iconMenu.setAttribute("src", "assets/images/menu.png");
     }
 
-   menu.classList.toggle('active');
+    menu.classList.toggle("active");
 });
